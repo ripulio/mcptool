eaa431b379c7a52014cc00cc84359c096b209c76
James Garbutt <43081j@users.noreply.github.com>
2025-10-23 16:18:50 +0100

    wip: add initial compile loading

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,41 @@ import ts from 'typescript';
 export interface CompilerOptions {
   outDir?: string;
   outExtension?: string;
+  cwd?: string;
+}
+
+function tryReadConfigFile(cwd: string): ts.CompilerOptions {
+  const configPath = ts.findConfigFile(cwd, ts.sys.fileExists, 'tsconfig.json');
+
+  if (!configPath) {
+    throw new Error('tsconfig.json not found');
+  }
+
+  const configFile = ts.readConfigFile(configPath, ts.sys.readFile);
+
+  if (configFile.error) {
+    const message = ts.flattenDiagnosticMessageText(
+      configFile.error.messageText,
+      '\n'
+    );
+    throw new Error(`Error reading tsconfig.json: ${message}`);
+  }
+
+  const parsedConfig = ts.parseJsonConfigFileContent(
+    configFile.config,
+    ts.sys,
+    path.dirname(configPath)
+  );
+
+  return parsedConfig.options;
 }
 
 export async function compileFile(
   filePath: string,
   options?: CompilerOptions
 ): Promise<void> {
-  const outDir = options?.outDir || '.';
+  const cwd = options?.cwd ?? process.cwd();
+  const outDir = path.resolve(cwd, options?.outDir || '.');
   const outExtension = options?.outExtension || '.mjs';
   const input = await readFile(filePath, 'utf-8');
   const extname = path.extname(filePath);
@@ -23,11 +51,38 @@ export async function compileFile(
   const outputName = path.basename(filePath, extname) + outExtension;
 
   const outputPath = path.join(outDir, outputName);
-  const output = await compile(input);
+  const output = await compile(input, options);
 
   await writeFile(outputPath, output);
 }
 
-export async function compile(input: string): Promise<string> {
+export async function compile(
+  input: string,
+  options?: CompilerOptions
+): Promise<string> {
+  const cwd = options?.cwd ?? process.cwd();
+  const fileName = 'input.ts';
+  const compilerOptions = tryReadConfigFile(cwd);
+
+  const host = ts.createCompilerHost(compilerOptions);
+
+  const originalReadFile = host.readFile;
+  host.readFile = (path) => {
+    if (path === fileName) {
+      return input;
+    }
+    return originalReadFile(path);
+  };
+
+  const originalFileExists = host.fileExists;
+  host.fileExists = (path) => {
+    if (path === fileName) {
+      return true;
+    }
+    return originalFileExists ? originalFileExists(path) : false;
+  };
+
+  const program = ts.createProgram([fileName], compilerOptions, host);
+
   return '';
 }
