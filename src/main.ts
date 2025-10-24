a9cabe98b51810d051a9b4622478d08f71c6d75e
James Garbutt <43081j@users.noreply.github.com>
2025-10-24 16:38:01 +0100

    wip: add error message

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,42 @@ export interface CompilerOptions {
   cwd?: string;
 }
 
+class InvalidParameterError extends Error {
+  constructor(
+    functionName: string,
+    paramName: string,
+    sourceFile: ts.SourceFile,
+    param: ts.ParameterDeclaration
+  ) {
+    const start = param.getStart(sourceFile);
+    const end = param.getEnd();
+    const {line, character} = sourceFile.getLineAndCharacterOfPosition(start);
+    const lineStart = sourceFile.getPositionOfLineAndCharacter(line, 0);
+
+    // Find the end of the line (next newline or end of file)
+    let lineEnd = sourceFile.text.indexOf('\n', lineStart);
+    if (lineEnd === -1) {
+      lineEnd = sourceFile.text.length;
+    }
+
+    const lineText = sourceFile.text.substring(lineStart, lineEnd);
+    const paramStart = character;
+    const paramEnd = character + (end - start);
+
+    const message = [
+      `\nError extracting function "${functionName}":`,
+      `  File: ${sourceFile.fileName}:${line + 1}:${character + 1}`,
+      `  ${lineText}`,
+      `  ${' '.repeat(paramStart)}${'~'.repeat(paramEnd - paramStart)}`,
+      `  Parameter "${paramName}" has an unsupported type.`,
+      `  Only plain objects, primitives, and arrays are allowed.\n`
+    ].join('\n');
+
+    super(message);
+    this.name = 'InvalidParameterError';
+  }
+}
+
 function isPlainObjectType(
   type: ts.Type,
   typeChecker: ts.TypeChecker
@@ -137,10 +173,11 @@ function visit(
           param.questionToken !== undefined || param.initializer !== undefined;
 
         if (!isPlainObjectType(paramType, typeChecker)) {
-          throw new Error(
-            `Parameter "${paramName}" in function "${functionName}" has an unsupported type. ` +
-              `Only plain objects, primitives, and arrays are allowed. ` +
-              `Complex types like class instances, Date, Map, Set, etc. are not supported.`
+          throw new InvalidParameterError(
+            functionName,
+            paramName,
+            node.getSourceFile(),
+            param
           );
         }
 
